5a6cb021775ebcd9db34177016dc817abaf2ea32
Albrecht Oster <albrecht@oster.online>
2020-09-14 18:14:11 +0200

    add event constraints

--- bot.rb
+++ bot.rb
@@ -6,13 +6,25 @@ require './stabi_api'
 require './booked_event'
 require './config'
 
-StabiApi.bookable_events.each do |event|
+events = StabiApi.bookable_events
+
+if events.none?
+  puts 'No open events found.'
+  exit true
+end
+
+events.each do |event|
   next if BookedEvent.exist? event[:id]
 
-  puts event
+  if event[:date] < DateTime.now || event[:date].hour < 13 || event[:date].wednesday?
+    puts "Skipping event with id=#{event[:id]} as it does not match constraints."
+    next
+  end
 
-  # StabiApi.book_event(event_id: events.first[:id],
-  #                     personal_info: Config.personal_info)
+  StabiApi.book_event(event_id: events.first[:id],
+                      personal_info: Config.personal_info)
 
   BookedEvent.create(event[:id])
+
+  puts "Booked event with id=#{event[:id]}."
 end
