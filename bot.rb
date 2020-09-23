7ea36f4c18d301601a5101bdb095ce6bf4220a62
Albrecht Oster <albrecht@oster.online>
2020-09-23 12:47:27 +0200

    move booking logic to a separate service object

--- bot.rb
+++ bot.rb
@@ -2,44 +2,18 @@
 
 require 'dotenv/load'
 
-require './stabi_api'
-require './booked_event'
-require './config'
+require './event_booker'
 require './logger'
 
 max_attempts = ARGV[0].to_i || 1
 attempt ||= 0
-events_booked = false
+booker = EventBooker.new
 
 loop do
   Logger.log "--- Attempt ##{attempt + 1} ---"
 
-  Logger.log 'Querying open events...'
-  events = StabiApi.bookable_events
-
-  Logger.log 'No open events found.' if events.none?
-
-  events.each do |event|
-    if BookedEvent.exist? event[:id]
-      Logger.log "Skipping already booked #{Logger.event_description(event)}."
-      next
-    end
-
-    if event[:date] < Date.today.next_day || event[:date].hour > 12
-      Logger.log "Skipping #{Logger.event_description(event)} as it does not match constraints."
-      next
-    end
-
-    Logger.log "Booking #{Logger.event_description(event)}..."
-    StabiApi.book_event(event_id: event[:id],
-                        personal_info: Config.personal_info)
-
-    BookedEvent.create(event[:id])
-    Logger.log "Successfully booked #{Logger.event_description(event)}."
-    events_booked = true
-  end
-
-  break unless (attempt += 1) < max_attempts && !events_booked
+  booked_events = booker.find_and_book_events
+  break unless (attempt += 1) < max_attempts && booked_events.none?
 
   sleep 1
 end
---
+++ event_booker.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
+require './stabi_api'
+require './config'
+require './logger'
+
+class EventBooker
+  EVENTS_FILE_PATH = 'booked_events.yml'
+
+  def find_and_book_events
+    bookable_events.each_with_object([]) do |event, booked_events|
+      next unless should_book_event?(event)
+
+      book_event(event)
+      booked_events << event
+    end
+  end
+
+  private
+
+  def bookable_events
+    log 'Querying open events...'
+    events = StabiApi.bookable_events
+    log 'No open events found.' if events.none?
+    events
+  end
+
+  def book_event(event)
+    log "Booking #{Logger.event_description(event)}..."
+    StabiApi.book_event(event_id: event[:id],
+                        personal_info: Config.personal_info)
+
+    add_booked_event(event)
+    log "Successfully booked #{Logger.event_description(event)}."
+  end
+
+  def should_book_event?(event)
+    if event_booked?(event)
+      log "Skipping already booked #{Logger.event_description(event)}."
+      return
+    end
+
+    if event_matches_contraints?(event)
+      log "Skipping #{Logger.event_description(event)} as it does not match constraints."
+      return
+    end
+
+    true
+  end
+
+  def event_booked?(event)
+    booked_event_ids.include? event[:id]
+  end
+
+  def event_matches_contraints?(event)
+    event[:date] < Date.today.next_day || event[:date].hour > 12
+  end
+
+  def add_booked_event(event)
+    return if Config.simulate_booking? || event_booked?(event)
+
+    booked_event_ids << event[:id]
+    persist_booked_event_ids
+  end
+
+  def booked_event_ids
+    @booked_event_ids ||= begin
+      if File.exist? EVENTS_FILE_PATH
+        events = YAML.safe_load(File.read(EVENTS_FILE_PATH))
+        raise 'booked_events.yml has invalid structure' unless (ids = events['ids']).is_a? Array
+
+        ids
+      else
+        []
+      end
+    end
+  end
+
+  def persist_booked_event_ids
+    File.open(EVENTS_FILE_PATH, 'w') do |file|
+      file.write({ 'ids' => booked_event_ids }.to_yaml)
+    end
+  end
+
+  def log(message)
+    Logger.log message
+  end
+end
